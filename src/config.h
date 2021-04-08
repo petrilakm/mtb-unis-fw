fea1ba288b226be0319095cf7c9124d9964b9f6e
Jan Horacek <me@apophis.cz>
2021-04-08 23:55:14 +0200

    FW v1.0.

--- src/config.h
+++ src/config.h
@@ -23,8 +23,8 @@ void config_boot_normal();
 uint8_t input_delay(uint8_t input);
 
 #define CONFIG_MODULE_TYPE 0x15
-#define CONFIG_FW_MAJOR 0
-#define CONFIG_FW_MINOR 1
+#define CONFIG_FW_MAJOR 1
+#define CONFIG_FW_MINOR 0
 #define CONFIG_PROTO_MAJOR 4
 #define CONFIG_PROTO_MINOR 0
 
