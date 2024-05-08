38a8d4eb392dcfa2a2151442e69a8600a26fa053
Jan Horacek <me@apophis.cz>
2024-05-08 19:13:39 +0200

    Reception of next message only after previous processed.

--- lib/mtbbus.c
+++ lib/mtbbus.c
@@ -96,10 +96,10 @@ void mtbbus_set_speed(uint8_t speed) {
 
 void mtbbus_update() {
 	if (received) {
-		received = false;
 		if (mtbbus_on_receive != NULL)
 			mtbbus_on_receive(received_addr == 0, mtbbus_input_buf[1],
 			                  (uint8_t*)mtbbus_input_buf+2, mtbbus_input_buf_size-3);
+		received = false; // must be after parsing (prevents changes in mtbbus_input_buf, mtbbus_input_buf_size)
 	}
 
 	if (sent) {
